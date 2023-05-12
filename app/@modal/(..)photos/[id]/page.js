f63ce2335dfd154f810a03a96d046c5358713f89
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2023-05-12 22:34:41 +0200

    Revert "update sample to demonstrate parallel route linking issue"
    This reverts commit b7e0ea4f1e8be44c21d3e7d8b5710500e9c08f08.

--- app/@modal/(..)photos/[id]/page.js
+++ app/@modal/(..)photos/[id]/page.js
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import Photo from "../../../../components/frame";
 import Modal from "../../../../components/modal";
 import swagPhotos from "../../../../photos";
@@ -10,7 +9,6 @@ export default function PhotoModal({ params: { id: photoId } }) {
   return (
     <Modal>
       <Photo photo={photo} />
-      <Link className="text-white" href="/about">About Us Page</Link>
     </Modal>
   );
 }
--- app/about/page.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="container mx-auto">
-      <h1>About Us Page</h1>
-    </main>
-  );
-}
