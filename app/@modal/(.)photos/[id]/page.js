03931cc75d4e397c21dd94abe1165798a4134175
Ali Idrizi <20397725+ali-idrizi@users.noreply.github.com>
2023-05-12 20:12:20 +0000

    Update 6 files

--- app/@modal/(.)photos/[id]/page.js
+++ app/@modal/(.)photos/[id]/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Photo from "../../../../components/frame";
 import Modal from "../../../../components/modal";
 import swagPhotos from "../../../../photos";
@@ -9,6 +10,7 @@ export default function PhotoModal({ params: { id: photoId } }) {
   return (
     <Modal>
       <Photo photo={photo} />
+      <Link className="text-white" href="/about">About Us Page</Link>
     </Modal>
   );
 }
